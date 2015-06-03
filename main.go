34055b49520bfd0924eda385acd6c50c74f1b960
Matt Aitchison <Matt@lanciv.com>
2015-06-03 10:19:18 -0500

    Added version and moved functions around.

--- main.go
+++ main.go
@@ -22,6 +22,8 @@ import (
 )
 
 var (
+	Version string
+
 	curUser, _ = user.Current()
 
 	sshPort    = env.Int("remotectl_port", 22, "ssh port to use. Note: all ports must be the same on hosts for a run.")
@@ -39,69 +41,6 @@ var (
 	profile     = flag.String("profile", "", "sources a bash profile to load a config") // Maybe a name will default to a file in ~/.remotectl
 )
 
-// type Host struct {
-// 	Name  string
-// 	Addr  string
-// 	Index int
-// 	Group int
-// }
-
-func runSSHCmd(host ext.Host, cfg *ssh.ClientConfig, group *sync.WaitGroup) {
-	defer group.Done()
-
-	// lgr := log.New(os.Stdout, host.Name, 0)
-	parts := strings.SplitN(host.Addr, ":", 1)
-	if len(parts) != 2 {
-		parts = append(parts, strconv.Itoa(sshPort))
-	}
-
-	hostAddr := strings.Join(parts, ":")
-	client, err := ssh.Dial("tcp", hostAddr, cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	session, err := client.NewSession()
-	if err != nil {
-		log.Fatal(err)
-	}
-	session.Stdout = os.Stdout
-	session.Stderr = os.Stderr
-
-	session.Run(strings.Join(os.Args[1:], " "))
-
-}
-
-// func hostsFromStdin() (hosts []string, ok bool) {
-// 	fi, err := os.Stdin.Stat()
-// 	if err != nil {
-// 		log.Fatal("error reading from stdin")
-// 	}
-// 	if fi.Mode()&os.ModeNamedPipe == 0 {
-// 		ok = false
-// 		return
-// 	}
-//
-// 	scanner := bufio.NewScanner(os.Stdin)
-// 	for scanner.Scan() {
-// 		re := regexp.MustCompile("(?s)#.*")
-// 		txt := re.ReplaceAllLiteralString(scanner.Text(), "")
-// 		txt = strings.TrimSpace(txt)
-// 		if len(txt) == 0 {
-// 			continue
-// 		}
-// 		log.Println(txt)
-// 		hosts = append(hosts, txt)
-// 	}
-// 	if err := scanner.Err(); err != nil {
-// 		// Assuming that an err scanning stdin is fatal and we
-// 		// shouldn't attempt to use a provider.
-// 		log.Fatal("reading standard input:", err)
-// 	}
-//
-// 	return hosts, true
-// }
-
 func main() {
 	log.SetFlags(log.Lshortfile)
 	flag.Parse()
@@ -111,16 +50,14 @@ func main() {
 		helpCmd()
 		os.Exit(0)
 	case *showVersion:
-		fmt.Println("remotectl: v0.1.0")
+		fmt.Printf("remotectl: %s", Version)
 		os.Exit(0)
 	}
 
 	hosts := []ext.Host{}
 	for _, p := range ext.Providers.All() {
 		p.Init()
-
 		extHosts, _ := p.Get()
-
 		hosts = append(hosts, extHosts...)
 	}
 
@@ -165,11 +102,40 @@ func newSSHClientConfig() (*ssh.ClientConfig, error) {
 	return cfg, nil
 }
 
+func runSSHCmd(host ext.Host, cfg *ssh.ClientConfig, group *sync.WaitGroup) {
+	defer group.Done()
+
+	// lgr := log.New(os.Stdout, host.Name, 0)
+	parts := strings.SplitN(host.Addr, ":", 1)
+	if len(parts) != 2 {
+		parts = append(parts, strconv.Itoa(sshPort))
+	}
+
+	hostAddr := strings.Join(parts, ":")
+	client, err := ssh.Dial("tcp", hostAddr, cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	session, err := client.NewSession()
+	if err != nil {
+		log.Fatal(err)
+	}
+	session.Stdout = os.Stdout
+	session.Stderr = os.Stderr
+
+	session.Run(strings.Join(os.Args[1:], " "))
+
+}
+
 func helpCmd() {
 	usage := `Usage: remotectl <flags> <query> [--] <cmd>
+
 Environment Vars:
 `
 
 	fmt.Printf(usage)
 	env.PrintDefaults()
+	fmt.Println("\nFlags:")
+	flag.PrintDefaults()
 }
