131c04821c1666d050398aebb7ff0996e7126f3d
Matt Aitchison <Matt@lanciv.com>
2015-06-16 15:14:03 -0500

    Cleaned up code

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"net"
 	"os"
 	"os/user"
 	"strings"
@@ -15,14 +14,13 @@ import (
 	"unicode"
 
 	ext "github.com/MattAitchison/remotectl/providers"
+	sshutil "github.com/MattAitchison/remotectl/ssh"
 
 	// Enabled Providers
 	_ "github.com/MattAitchison/remotectl/digitalocean"
 	_ "github.com/MattAitchison/remotectl/stdin"
 
 	env "github.com/MattAitchison/envconfig"
-	"golang.org/x/crypto/ssh"
-	"golang.org/x/crypto/ssh/agent"
 )
 
 var (
@@ -56,6 +54,24 @@ func fatalErr(err error) {
 	}
 }
 
+func fields(s string) []string {
+	f := func(c rune) bool {
+		return c == ',' || unicode.IsSpace(c)
+	}
+	return strings.FieldsFunc(s, f)
+}
+
+// func parseArgs(arg ...string) (string, string) {
+// 	flag.
+// 	for i, a := range arg {
+//
+// 	}
+// }
+// func setupProviders(provider []string) {
+//
+// }
+// func queryProviders(provider []string)
+
 func main() {
 	// Show filename and line number in logs.
 	log.SetFlags(log.Lshortfile)
@@ -70,23 +86,22 @@ func main() {
 		os.Exit(0)
 	}
 
+	query := flag.Args()[0]
+	cmd := strings.Join(flag.Args()[1:], " ")
+
 	var hosts []ext.Host
 	// Get all of the hosts from each provider.
 	// This will have a way to filter soon.
-	f := func(c rune) bool {
-		return c == ',' || unicode.IsSpace(c)
-	}
-	fmt.Println(strings.FieldsFunc(provider, f))
-	for _, p := range ext.Providers.Select(strings.FieldsFunc(provider, f)) {
+	for _, p := range ext.Providers.Select(fields(provider)) {
 		if p == nil {
-			log.Fatal("provider undefined")
+			log.Fatal("unknown provider")
 		}
 
 		// Setup the provider
 		p.Setup()
 
 		// Query the provider for hosts
-		extHosts, err := p.Query(namespace, "")
+		extHosts, err := p.Query(namespace, query)
 		fatalErr(err)
 		hosts = append(hosts, extHosts...)
 	}
@@ -100,40 +115,67 @@ func main() {
 		return
 	}
 
-	cfg, err := newSSHClientConfig()
+	cfg, err := sshutil.NewClientConfig(usr)
 	fatalErr(err)
 
-	cmd := strings.Join(flag.Args(), " ")
-	group := &sync.WaitGroup{}
+	var sessions []*sshutil.Session
 	for _, host := range hosts {
-		group.Add(1)
-		go runSSHCmd(group, cfg, host, cmd)
+		addr := fmt.Sprint(host.Addr, ":", sshPort)
+		sess, err := cfg.NewSession(addr)
+		fatalErr(err)
+
+		outPipe, err := sess.StdoutPipe()
+		fatalErr(err)
+
+		errPipe, err := sess.StderrPipe()
+		fatalErr(err)
+
+		go hostStreamer(host, outPipe, os.Stdout)
+		go hostStreamer(host, errPipe, os.Stderr)
+		sessions = append(sessions, sess)
 	}
 
-	// Wait until all ssh cmds are done running.
+	if len(sessions) == 0 {
+		log.Fatal("no sessions")
+	}
+
+	group := &sync.WaitGroup{}
+	for _, s := range sessions {
+		group.Add(1)
+		go s.RunWaitGroup(group, cmd)
+	}
 	group.Wait()
+
+	// group := &sync.WaitGroup{}
+	// for _, host := range hosts {
+	// 	group.Add(1)
+	// 	go runSSHCmd(group, cfg.ClientConfig, host, cmd)
+	// }
+	//
+	// // Wait until all ssh cmds are done running.
+	// group.Wait()
 }
 
 // newSSHClientConfig returns a config using an ssh agent.
-func newSSHClientConfig() (*ssh.ClientConfig, error) {
-	sock, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK"))
-	if err != nil {
-		return nil, err
-	}
-
-	agent := agent.NewClient(sock)
-	signers, err := agent.Signers()
-	if err != nil {
-		return nil, err
-	}
-
-	cfg := &ssh.ClientConfig{
-		User: usr,
-		Auth: []ssh.AuthMethod{ssh.PublicKeys(signers...)},
-	}
-
-	return cfg, nil
-}
+// func newSSHClientConfig() (*ssh.ClientConfig, error) {
+// 	sock, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK"))
+// 	if err != nil {
+// 		return nil, err
+// 	}
+//
+// 	agent := agent.NewClient(sock)
+// 	signers, err := agent.Signers()
+// 	if err != nil {
+// 		return nil, err
+// 	}
+//
+// 	cfg := &ssh.ClientConfig{
+// 		User: usr,
+// 		Auth: []ssh.AuthMethod{ssh.PublicKeys(signers...)},
+// 	}
+//
+// 	return cfg, nil
+// }
 
 func hostStreamer(host ext.Host, r io.Reader, w io.Writer) {
 	// Locking? One writer?
@@ -141,40 +183,40 @@ func hostStreamer(host ext.Host, r io.Reader, w io.Writer) {
 
 	for scanner.Scan() {
 		prefixTmpl.Execute(w, host)
-		fmt.Printf("%s \n", scanner.Text())
+		fmt.Println(scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
-		log.Fatalf("Error while reading from Writer: %s", err)
+		log.Fatalf("err streaming logs %s", err)
 	}
 
 }
 
-func runSSHCmd(group *sync.WaitGroup, cfg *ssh.ClientConfig, host ext.Host, cmd string) {
-	defer group.Done()
-
-	// Append port to host.Addr
-	hostAddr := fmt.Sprintf("%s:%v", host.Addr, sshPort)
-	client, err := ssh.Dial("tcp", hostAddr, cfg)
-	fatalErr(err)
-
-	// Create an ssh session.
-	session, err := client.NewSession()
-	fatalErr(err)
-	defer session.Close()
-
-	// Setup host streaming
-	outPipe, err := session.StdoutPipe()
-	fatalErr(err)
-
-	errPipe, err := session.StderrPipe()
-	fatalErr(err)
-
-	go hostStreamer(host, outPipe, os.Stdout)
-	go hostStreamer(host, errPipe, os.Stderr)
-
-	session.Run(cmd)
-}
+// func runSSHCmd(group *sync.WaitGroup, cfg *ssh.ClientConfig, host ext.Host, cmd string) {
+// 	defer group.Done()
+//
+// 	// Append port to host.Addr
+// 	hostAddr := fmt.Sprintf("%s:%v", host.Addr, sshPort)
+// 	client, err := ssh.Dial("tcp", hostAddr, cfg)
+// 	fatalErr(err)
+//
+// 	// Create an ssh session.
+// 	session, err := client.NewSession()
+// 	fatalErr(err)
+// 	defer session.Close()
+//
+// 	// Setup host streaming
+// 	outPipe, err := session.StdoutPipe()
+// 	fatalErr(err)
+//
+// 	errPipe, err := session.StderrPipe()
+// 	fatalErr(err)
+//
+// 	go hostStreamer(host, outPipe, os.Stdout)
+// 	go hostStreamer(host, errPipe, os.Stderr)
+//
+// 	session.Run(cmd)
+// }
 
 func helpCmd() {
 	usage := `Usage: remotectl <flags> <query> [--] <cmd>
